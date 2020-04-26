57758794544d1b25c662441845292457f8fb3fb9
Emilian Brenici <brenici@gmail.com>
2020-04-26 18:27:33 +0200

    Add Auto Layout Constraints

--- ColorPicker/SceneDelegate.swift
+++ ColorPicker/SceneDelegate.swift
@@ -2,8 +2,8 @@
 //  SceneDelegate.swift
 //  ColorPicker
 //
-//  Created by E.Brenici on 26/04/2020.
-//  Copyright © 2020 emilianbrenici. All rights reserved.
+//  Created by Emilian Brenici on 26/04/2020.
+//  Copyright © 2020 Emilian Brenici. All rights reserved.
 //
 
 import UIKit
@@ -12,8 +12,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+//        print("Function: \(#function), line: \(#line)")
+
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
@@ -21,6 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
+//        print("Function: \(#function), line: \(#line)")
+
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
@@ -28,26 +31,29 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
+//        print("Function: \(#function), line: \(#line)")
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
+//        print("Function: \(#function), line: \(#line)")
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
+//        print("Function: \(#function), line: \(#line)")
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
+//        print("File: \((#file as NSString).lastPathComponent), function: \(#function), line: \(#line)")
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 
 }
 
--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -2,19 +2,40 @@
 //  ViewController.swift
 //  ColorPicker
 //
-//  Created by E.Brenici on 26/04/2020.
-//  Copyright © 2020 emilianbrenici. All rights reserved.
+//  Created by Emilian Brenici on 26/04/2020.
+//  Copyright © 2020 Emilian Brenici. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorPickerView: UIView!
+    
+    let fileName = (#file as NSString).lastPathComponent
+    var didUpdateLayout = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("File: \(fileName), func: \(#function), line: \(#line)")
+        colorPickerView.translatesAutoresizingMaskIntoConstraints = false
     }
 
-
+    override func viewWillLayoutSubviews() {
+        print("File: \(fileName), func: \(#function), line: \(#line)")
+        if !didUpdateLayout {
+            updateLayout()
+        }
+    }
+    
+    private func updateLayout() {
+        print("File: \(fileName), func: \(#function), line: \(#line)")
+        let side = min(self.view.frame.height, self.view.frame.width)
+        let constant = side > 414 ? side * 0.6 : side
+        print(constant)
+        colorPickerView.widthAnchor.constraint(equalToConstant: constant).isActive = true
+        colorPickerView.heightAnchor.constraint(equalToConstant: constant).isActive = true
+        didUpdateLayout = true
+    }
+    
 }
-
